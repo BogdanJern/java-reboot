58cb79b613e29b06d19f409bffb010be6a33016e
bogdanzernovoj <jernowoi.bogdan@yandex.ru>
2023-11-20 20:19:26 +0300

    Реализовал класс CityInfo.

--- module05/src/main/java/ru/sberbank/edu/App.java
+++ module05/src/main/java/ru/sberbank/edu/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args ) {
         GeoPosition pos = new GeoPosition("-55(45'07'')","59");
         System.out.println(pos.toString());
+        CityInfo city = new CityInfo("Kasimov", pos);
+        System.out.println(city.toString());
     }
 }
