93d2147c60e72b73678c5ea5a4fdaf1554031315
bogdanzernovoj <jernowoi.bogdan@yandex.ru>
2023-11-20 20:14:47 +0300

    Реализовал класс GeoPosition.

--- module05/src/main/java/ru/sberbank/edu/App.java
+++ module05/src/main/java/ru/sberbank/edu/App.java
@@ -6,8 +6,8 @@ package ru.sberbank.edu;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        GeoPosition pos = new GeoPosition("-55(45'07'')","59");
+        System.out.println(pos.toString());
     }
 }
--- module05/src/main/java/ru/sberbank/edu/GeoPosition.java
+++ module05/src/main/java/ru/sberbank/edu/GeoPosition.java
@@ -1,5 +1,10 @@
 package ru.sberbank.edu;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Math.PI;
+
 /**
  * Geo position.
  */
@@ -15,6 +20,8 @@ public class GeoPosition {
      */
     private double longitude;
 
+    private final String PATTERN = "^(-?)([0-9]{1,3})(\\(([0-6]{1,2})\'([0-9]{1,2})\'\'\\))?$";
+
     /**
      * Ctor.
      *
@@ -23,8 +30,48 @@ public class GeoPosition {
      *                        Possible values: 55, 55(45'07''), 59(57'00'')
      */
     public GeoPosition(String latitudeGradus, String longitudeGradus) {
-        // parse and set latitude and longitude in radian
+
+        latitude = gradus(latitudeGradus);
+        longitude = gradus(longitudeGradus);
+
     }
 
-    // getters and toString
+    public double gradus(String gradus) {
+        double radGrad = 0.0, minutes = 0.0, seconds = 0.0;
+        Pattern regex = Pattern.compile(PATTERN);
+        Matcher matcher = regex.matcher(gradus);
+        if (!matcher.find()) {
+
+        }
+
+        radGrad = Double.parseDouble(matcher.group(2));
+        if(matcher.group(1) != ""){
+            radGrad = radGrad * -1.0;
+        }
+        radGrad = radGrad * PI / 180;
+
+        if (matcher.group(4) != null && matcher.group(4) != "00" ){
+            radGrad = radGrad + ( Double.parseDouble(matcher.group(2)) * PI / 180 / 60.0 );
+        }
+        if (matcher.group(5) != null && matcher.group(5) != "00" ){
+            radGrad = radGrad + ( Double.parseDouble(matcher.group(2)) * PI / 180 / 3600.0 );
+        }
+        return radGrad;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "GeoPosition{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
 }
