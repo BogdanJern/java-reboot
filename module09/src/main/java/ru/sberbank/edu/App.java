dad6cce1edb6a670bdb820e4896f97bfc7840522
bogdanzernovoj <jernowoi.bogdan@yandex.ru>
2023-12-18 11:43:32 +0300

    Добавлена зависимость springframework. и класс Config

--- module09/src/main/java/ru/sberbank/edu/App.java
+++ module09/src/main/java/ru/sberbank/edu/App.java
@@ -1,5 +1,8 @@
 package ru.sberbank.edu;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new AnnotationConfigApplicationContext("ru.sberbsnk.edu");
+
+        WeatherCache cache = context.getBean(WeatherCache.class);
+
+        WeatherInfo weatherInfo = cache.getWeatherInfo("OMSK");
+        System.out.println("GOOD! weather=" + weatherInfo);
+
     }
 }
