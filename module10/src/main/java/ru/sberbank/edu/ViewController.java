dd74f4350210e2a62053c8526765b6db81592156
bogdanzernovoj <jernowoi.bogdan@yandex.ru>
2024-01-03 15:15:41 +0530

    Исправлено расположение файлов

--- module10/src/main/java/ru/sberbank/edu/ViewController.java
+++ module10/src/main/java/ru/sberbank/edu/ViewController.java
@@ -1,25 +1,23 @@
-package ru.sberbank.edu.controller;
+package ru.sberbank.edu;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/finance", consumes = MediaType.ALL_VALUE)
 public class ViewController{
     @GetMapping()
     public String finance(){
-        return "ru/sberbank/edu/finance.html";
-        //ModelAndView modelAndView = new ModelAndView();
-        //modelAndView.setViewName("/finance.html");
-        //return modelAndView;
+        return "finance.html";
     }
 
     @PostMapping
-    public String financePage(){
-        return "ru/sberbank/edu/finance.html";
+    public String result(
+
+    ){
+        return "finance.html";
     }
 }
