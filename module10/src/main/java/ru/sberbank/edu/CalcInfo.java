ea8428153e31fa2fc89ffbec0d98d8c9106c65de
bogdanzernovoj <jernowoi.bogdan@yandex.ru>
2024-01-03 15:32:21 +0530

    Добавлен класс CalcInfo и параметр minSum с минимальной суммой

---
+++ module10/src/main/java/ru/sberbank/edu/CalcInfo.java
@@ -0,0 +1,34 @@
+package ru.sberbank.edu;
+
+public class CalcInfo {
+    /**
+     * Сумма вклада
+     */
+    private final Double sum;
+    /**
+     * Процентная ставка
+     */
+    private final Double rate;
+    /**
+     * Количество лет
+     */
+    private final int years;
+
+    public CalcInfo(Double sum, Double rate, int years) {
+        this.sum = sum;
+        this.rate = rate;
+        this.years = years;
+
+        if(this.sum < 50000.0){
+            throw new IllegalAccessError("Минимальная сумма на момент открытия вклада 50 000 рублей");
+        }
+    }
+
+    /**
+     * Расчет суммы вклада
+     * @return
+     */
+    public Double getFinallSum(){
+        return sum*Math.pow(1+rate/100,years);
+    }
+}
--- module10/src/main/java/ru/sberbank/edu/ViewController.java
+++ module10/src/main/java/ru/sberbank/edu/ViewController.java
@@ -1,10 +1,12 @@
 package ru.sberbank.edu;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/finance", consumes = MediaType.ALL_VALUE)
@@ -16,8 +18,20 @@ public class ViewController{
 
     @PostMapping
     public String result(
+            @RequestParam(value = "sum", required = true) String sum,
+            @RequestParam(value = "percentage", required = true) String percentage,
+            @RequestParam(value = "years", required = true) String years,
+            @Value("${minSum}") int minLimi ) {
 
-    ){
-        return "finance.html";
+        try {
+            CalcInfo calcInfo = new CalcInfo(Double.parseDouble(sum),
+                    Double.parseDouble(percentage),
+                    Integer.parseInt(years));
+            return "";
+        } catch (NumberFormatException e) {
+            return "";
+        } catch (IllegalAccessError e) {
+            return "";
+        }
     }
 }
