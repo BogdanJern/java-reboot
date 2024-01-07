76bb695e64c921c035f4c991c4f06a2cc937e5ff
BogdanJern <jernowoi.bogdan@yandex.ru>
2024-01-07 19:16:09 +0530

    Реализован класс User.

--- module11/src/main/java/com/example/module11/entity/User.java
+++ module11/src/main/java/com/example/module11/entity/User.java
@@ -1,8 +1,47 @@
 package com.example.module11.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @Column(name = "id")
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "age")
     private Integer age;
+
+    public User(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
