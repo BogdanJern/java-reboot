158db5120831d8381482191c7de8c2c1dd6bd2ae
BogdanJern <jernowoi.bogdan@yandex.ru>
2024-01-14 12:41:31 +0530

    Реализовал Config

---
+++ module12/src/main/java/ru/edu/module12/config/Config.java
@@ -0,0 +1,26 @@
+package ru.edu.module12.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.example.module12.repository")
+@EnableTransactionManagement
+public class Config {
+    @Bean
+    public DataSource dataSource() {
+        try {
+            var dbBuilder = new EmbeddedDatabaseBuilder();
+            return dbBuilder.setType(EmbeddedDatabaseType.H2)
+                    .build();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
